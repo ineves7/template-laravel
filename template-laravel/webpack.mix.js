8601ae25504e0aad39e57229f9c8118c7af2ebfc
IAGO NEVES <iago.neves-ms@hotmail.com>
2021-11-10 16:37:08 -0300

    import app

--- template-laravel/webpack.mix.js
+++ template-laravel/webpack.mix.js
@@ -14,3 +14,7 @@ const mix = require("laravel-mix");
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/css/app.scss", "public/css")
     .version();
+
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/css/app.scss', 'public/css');
